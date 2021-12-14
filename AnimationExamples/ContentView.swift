f21c191d5e902ac0f799d20812a33e8832d14c9e
lcs-ashin <ashin@lcs.on.ca>
2021-12-14 15:27:30 -0500

    Animated bindings.

--- AnimationExamples/ContentView.swift
+++ AnimationExamples/ContentView.swift
@@ -12,26 +12,19 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     
     var body: some View {
-        Button("Tap Me") {
-          //  animationAmount += 1
-        }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .animation(
-                    .easeInOut(duration: 1)
-                    .repeatForever(autoreverses: false),
-                    value: animationAmount
-                )
-        )
-        .onAppear {
-            animationAmount = 2
+        VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
+            
+            Spacer()
+            
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
         }
     }
 }
