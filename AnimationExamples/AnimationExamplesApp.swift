36f415a9337ad6e75f05793ae320a43cd97864d4
lcs-ashin <ashin@lcs.on.ca>
2021-12-14 14:31:39 -0500

    Initial Commit

---
+++ AnimationExamples/AnimationExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationExamplesApp.swift
+//  AnimationExamples
+//
+//  Created by Eunbi Shin on 2021-12-14.
+//
+
+import SwiftUI
+
+@main
+struct AnimationExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
